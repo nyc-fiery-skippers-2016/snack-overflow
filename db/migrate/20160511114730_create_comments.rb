6141997f05daa013baac4911d4e8eb3c77ac03b5
sergener <sibel.ergener@gmail.com>
2016-05-11 12:10:54 -0400

    Add migration tables to users, questions, and comments

--- db/migrate/20160511114730_create_comments.rb
+++ db/migrate/20160511114730_create_comments.rb
@@ -1,4 +1,12 @@
 class CreateComments < ActiveRecord::Migration
   def change
+  	create_table :comments do |t|
+  		t.string :body, null: false
+  		t.references :user, foreign_key: true, null: false
+  		t.integer :commentable_id, polymorphic: true
+  		t.string :commentable_type, polymorphic: true
+
+  		t.timestamps(null: false)
+  	end
   end
 end
