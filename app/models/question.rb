f036793047f656082a97888bc499c484724e66c4
DianaEromosele <diana.ozemebhoya@gmail.com>
2016-05-11 15:31:19 -0400

    Add Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
+  has_many :answers
+  has_many :comments
+  has_many :tags, through: :question_tags
+
 end
