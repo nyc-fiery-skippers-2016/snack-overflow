42a1d5e0a6085ec4719d8243060a458bf49cda7e
sergener <sibel.ergener@gmail.com>
2016-05-11 12:25:40 -0400

    Add completed migrations

--- db/migrate/20160511114741_create_votes.rb
+++ db/migrate/20160511114741_create_votes.rb
@@ -2,9 +2,9 @@ class CreateVotes < ActiveRecord::Migration
   def change
   	create_table :votes do |t|
   		t.integer :value || 0
-  		t.references :user
-  		t.integer :votable_id, polymorphic: true
-  		t.string :votable_type, polymorphic: true
+  		t.references :user, foreign_key: true, null: false
+  		t.integer :votable_id, polymorphic: true, null: false
+  		t.string :votable_type, polymorphic: true, null: false
 
   		t.timestamps(null: false)
   	end
--- db/migrate/20160511114825_create_answers.rb
+++ db/migrate/20160511114825_create_answers.rb
@@ -1,4 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+  	create_table :answers do |t|
+  		t.string :body, null: false
+  		t.reference :user, foreign_key: true, null: false
+  		t.reference :question, foreign_key: true, null: false
+
+  		t.timestamps(null: false)
+  	end
   end
 end
