8da47dea4774a5c2fcb4d2f1f94549dc9f63e2dd
sergener <sibel.ergener@gmail.com>
2016-05-11 15:59:58 -0400

    Tweaks to seeds, tag model, and create user controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,25 +4,26 @@ User.delete_all
 Tag.delete_all
 
 users = 10.times.map do
-  User.create!( :username 		=> Faker::Superhero,
+  User.create!( :username 		=> Faker::Name.name,
                 :email      	=> Faker::Internet.email,
                 :password   	=> 'password')
 end
 
 questions = 10.times.map do
-	Question.create!(:title      => Faker::Book.title,
+	Question.create!(:title      => Faker::Lorem.sentence,
                   :body        => Faker::Lorem.paragraph)
                  
 end
 
 answers = 10.times.map do
 	Answer.create!(:body					=> Faker::Hacker.say_something_smart,
-								 :user_id				=> rand(1..10)
+								 :user_id				=> rand(1..10),
 								 :question_id		=> rand(1..10)  )
 end
 
 tags = [%w(salty sweet savory umami yum gross bleh morning afternoon midnight diet gluten )].map do |tag|
 		Tag.create!(:category 			=> tag)
+	end
 
 
 
