862dfaa4af77741cd35403fc4024d2149602baf1
sergener <sibel.ergener@gmail.com>
2016-05-11 12:15:15 -0400

    Add migrations got votes and tags

--- db/migrate/20160511114741_create_votes.rb
+++ db/migrate/20160511114741_create_votes.rb
@@ -1,4 +1,12 @@
 class CreateVotes < ActiveRecord::Migration
   def change
+  	create_table :votes do |t|
+  		t.integer :value || 0
+  		t.references :user
+  		t.integer :votable_id, polymorphic: true
+  		t.string :votable_type, polymorphic: true
+
+  		t.timestamps(null: false)
+  	end
   end
 end
