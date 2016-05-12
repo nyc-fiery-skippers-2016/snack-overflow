8a24f80003285d60ac289efcd84812f0b120bf37
Diana Ozemebhoya Eromosele <email": null>
2016-05-12 16:02:23 -0400

    Add Answer Controller and main photo for homepage

--- app/controllers/answers_controllers.rb
+++ app/controllers/answers_controllers.rb
@@ -1,54 +1,64 @@
 # list of answers to a question
 get '/questions/:question_id/answers' do
   @question = Question.find(params[:question_id])
-  @answer = Answer.find_by(question_id: :question_id)
   erb :'answers/index'
 end
 
+#display a specific answer to a question
+get '/questions/:question_id/answers/:id' do
+   @question = Question.find(params[:question_id])
+   @answer = Answer.find(params[:id])
+   erb :'answers/show'
+end
+
+#submit a new answer. 2 steps:
+
 #1) get a form to submit an answer
 get '/questions/:question_id/answers/new' do
-  @question = Question.find(params[:id])
+  @question = Question.find(params[:question_id])
   erb :'answers/new'
 end
 
-#2) execute intel received from form
+#2) execute intel received from new-answer form
 post '/questions/:question_id/answers/new' do
-  @question = Question.find(params[:id])
-  if logged_in?
-   @answer = Answer.new(body: params[:body], user_id: @question.user_id, question_id: @question.id)
+  # @question = Question.find(params[:question_id])
+  # if logged_in?
+   @answer = Answer.new(body: params[:body], user_id: current_user.id, question_id: params[:question_id])
     if @answer.save
-      redirect '/questions/#{@question.id}/answers}'
+      redirect "/questions/#{@question.id}/answers"
     else
       erb :'answers/new'
     end
-  else
-    erb :'sessions/new'
-    ## error telling them to sign in
-  end
+  # else
+  #   erb :'sessions/new'
+  #   ## error telling them to sign in
+  # end
 end
 
 #edit answer entry. 2 steps:
 
 #1) get an answer form to edit
-get '/questions/:question_id_/answers/:id/edit' do
-  @answer = Answer.find_by(id: params[:id])
+get '/questions/:question_id/answers/:id/edit' do
+  # @question = Question.find_by(id: params[:question_id])
+  @answer = Answer.find(params[:id])
   erb :'/answers/edit'
 end
 
 #2) run the intel received from an answer edit form
-post '/questions/:question_id_/answers/:id/edit' do
-  if logged_in?
-   @answer = Answer.find_by(id: params[:id])
-    @answer.assign_attributes(body: params[:body])
+put '/questions/:question_id/answers/:id/edit' do
+  # if logged_in?
+   # @question = Question.find_by(question_id: params[:question_id])
+   @answer = Answer.find_by(id: params[:answer][:id])
+    @answer.assign_attributes(body: params[:answer][:body])
     if @answer.save
-      redirect "/questions/#{@question.id}/answers"
+      redirect "/questions/#{@answer.question_id}/answers"
     else
-      erb 'answers/edit'
+      erb :'/answers/edit'
     end
-  else
-    erb :'sessions/new'
-    #throws error telling them to sign in
-  end
+  # else
+  #   erb :'sessions/new'
+  #   #want to throw error telling them to sign in
+  # end
 end
 
 #delete an answer
