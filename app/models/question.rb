4785ece0978b1518315c9df337cace370acac3db
RobertReith <robertmreith@gmail.com>
2016-05-12 10:30:00 -0400

    Update association for comments on a question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
   has_many :votes, as: :voteable
-  has_many :comments
+  has_many :comments, as: :commentable
   has_many :tags, through: :question_tags
 
 end
